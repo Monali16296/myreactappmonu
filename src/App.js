dfc6fbf065875de3357fff07142eed887aa4d472
Monali Hingu <monali16296@gmail.com>
2019-04-11 22:29:47 +0530

    Modified my basic program.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-      hello Monali<br/>
-      Welcome to React
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+        </header>
       </div>
     );
   }
