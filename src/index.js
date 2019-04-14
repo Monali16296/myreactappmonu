3f9fffcf3ad01f5718742854eb2f073fb69c7ca0
Monali Hingu <monali16296@gmail.com>
2019-04-14 10:58:29 +0530

    Embedding expressions in jsx

--- src/index.js
+++ src/index.js
@@ -6,8 +6,21 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+/*
+without using external component
+*/
 ReactDOM.render(
   <h1>Hi Monali</h1>,
   document.getElementById('my-root')
 );
+
+/*
+embedding expressions in jsx
+*/
+const name = "Mohit";
+const element = <h1> Hey {name}</h1>;
+
+ReactDOM.render(element, document.getElementById('root1'));
+
 serviceWorker.unregister();
+
