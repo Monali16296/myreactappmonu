af1ef3ba1cf455ac3a83eaa393540e424d703f79
Monali Hingu <monali16296@gmail.com>
2019-04-14 11:01:52 +0530

    can also put any valid javascript expression in curly braces

--- src/index.js
+++ src/index.js
@@ -22,5 +22,12 @@ const element = <h1> Hey {name}</h1>;
 
 ReactDOM.render(element, document.getElementById('root1'));
 
+/*
+can also put any valid javascript expression in curly braces
+*/
+const element2 = <h1> total {2+2}</h1>;
+
+ReactDOM.render(element2, document.getElementById('root2'));
+
 serviceWorker.unregister();
 
